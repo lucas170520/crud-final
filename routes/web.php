5c5bf1735a7ccc596de380918ea7dbd0061300e7
Lucas <Lucas@123>
2025-12-02 14:20:43 -0300

    feat: adiciona rota de instrumentos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InstrumentoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('instrumentos', InstrumentoController::class);
