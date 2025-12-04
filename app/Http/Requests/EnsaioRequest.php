eba8ebef10c104f7cc046dc2c6296542e06be528
lucaxx <lucas.medeiros15@aluno.ce.gov.br>
2025-12-04 15:59:03 -0300

    feat: cria ensaio request

---
+++ app/Http/Requests/EnsaioRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EnsaioRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'data' => 'required|date',
+            'local' => 'required|string|max:50',
+            'horario' => 'required|date_format:H:i',
+        ];
+    }
+}
