03a649ccfd0c2b1bf81450b795c8a2612680209b
lucaxx <lucas.medeiros15@aluno.ce.gov.br>
2025-12-04 14:23:18 -0300

    feat: adiciona rota para crud integrantes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InstrumentoController;
+use App\Http\Controllers\IntegranteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('instrumentos', InstrumentoController::class);
+Route::resource('integrantes', IntegranteController::class);
