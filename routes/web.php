f6dbe1c9efb16b140d29bc929ad41331b8e80d4a
Lucax <Lucaxx@gmail>
2025-12-08 14:04:11 -0300

    feat: adiciona laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InstrumentoController;
 use App\Http\Controllers\IntegranteController;
@@ -11,8 +12,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('instrumentos', InstrumentoController::class);
-Route::resource('integrantes', IntegranteController::class);
-Route::resource('musicas', MusicaController::class);
-Route::resource('ensaios', EnsaioController::class);
-Route::resource('eventos', EventoController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('instrumentos', InstrumentoController::class);
+    Route::resource('integrantes', IntegranteController::class);
+    Route::resource('musicas', MusicaController::class);
+    Route::resource('ensaios', EnsaioController::class);
+    Route::resource('eventos', EventoController::class);
+});
+
+require __DIR__.'/auth.php';
